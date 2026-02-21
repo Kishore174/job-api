75dfbeee3818acb21795ffb0082ecc5029d463ae
Kishore174 <skishore174@gmail.com>
2026-02-21 15:17:16 +0530

    emailtemp1

--- src/config/email.js
+++ src/config/email.js
@@ -1,10 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true, // true for port 465
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.EMAIL_USER,   // careers@crewminds.com
+    pass: process.env.EMAIL_PASS    // password you created in Hostinger
   }
 });
 
